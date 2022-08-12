834dbe1a3a9ba97e98eb140a44f8b605078697ae
nova-sangeeth <novasangeeth@outlook.com>
2022-08-13 00:00:33 +0530

    test model for debug purposes.

--- debug_django/authentication/models.py
+++ debug_django/authentication/models.py
@@ -1,3 +1,14 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+
+
+class TestModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "test_models"
