97b03806e5c7267e0e58c3f951d20cfbc6cc09bf
nova-sangeeth <novasangeeth@outlook.com>
2022-08-13 00:00:55 +0530

    debugging purposes views.

--- debug_django/authentication/views.py
+++ debug_django/authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from authentication.models import TestModel
+
+
+def index(request):
+    testQuerySet = TestModel.objects.all().values()
+    return testQuerySet
